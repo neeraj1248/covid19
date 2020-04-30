c7cb74eda0c014a702137103afac38769435112b
Neeraj <neerajkumar1248.nk@gmail.com>
2020-04-30 22:35:47 +0530

    change 2

--- covid19status/settings.py
+++ covid19status/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'covid19status.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['TEMPLATES'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
